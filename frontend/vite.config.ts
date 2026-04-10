c82e59d61d573478e73259e01c2d87ad38daa834
Your Name <you@example.com>
2026-04-10 15:35:43 +0530

    chore: update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, "../", "");
+  return {
+    envDir: "../",
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: Number(env.FRONTEND_PORT),
+      host: env.FRONTEND_DOMAIN,
+    },
+  }
 });
