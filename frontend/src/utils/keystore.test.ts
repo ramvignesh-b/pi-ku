8d431d6619793a7f9a7d69cbac152bb657330064
ramvignesh-b <ramvignesh-b@github.com>
2026-04-12 16:21:22 +0530

    chore: emove App.css, and fix type assertion in keystore test

--- frontend/src/utils/keystore.test.ts
+++ frontend/src/utils/keystore.test.ts
@@ -46,7 +46,10 @@ describe("keystore", () => {
     await saveMasterKey(key1);
     await saveMasterKey(key2);
     const loadedKey = await loadMasterKey();
-    const loadedJwk = await crypto.subtle.exportKey("jwk", loadedKey!);
+    const loadedJwk = await crypto.subtle.exportKey(
+      "jwk",
+      loadedKey as CryptoKey,
+    );
     const key1Jwk = await crypto.subtle.exportKey("jwk", key1);
     const key2Jwk = await crypto.subtle.exportKey("jwk", key2);
 
