b5d55bd2580f0f97297dbaa7707d0d6f2b0a0980
ramvignesh-b <ramvignesh-b@github.com>
2026-04-21 01:49:15 +0530

    feat: update splash screen elements

--- frontend/src/components/SplashScreen.tsx
+++ frontend/src/components/SplashScreen.tsx
@@ -6,11 +6,17 @@ export default function SplashScreen() {
     <div className="fixed w-screen h-screen inset-0 bg-base-100 flex flex-col items-center justify-center z-9999">
       <div className="flex flex-col items-center gap-6 animate-pulse">
         <Logo />
+
         <div className="flex flex-col items-center gap-2">
-          <EnvelopeOpenIcon weight="duotone" fill="primary" size={12} />
-          <span className="loading loading-ring loading-lg text-primary" />
-          <p className="text-xs uppercase font-sans tracking-widest opacity-40">
-            Unsealing...
+          <EnvelopeOpenIcon
+            weight="thin"
+            className={"absolute text-primary/50"}
+            size={40}
+          />
+          <span className="loading loading-ring loading-xl text-primary"></span>
+          ...
+          <p className="text-xs uppercase font-sans tracking-[1em] opacity-40">
+            Unsealing
           </p>
         </div>
       </div>
