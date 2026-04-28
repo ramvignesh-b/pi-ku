dc0d68888530481150a9abeaaca06c500b3232ef
ramvignesh-b <ramvignesh-b@github.com>
2026-04-29 01:14:11 +0530

    chore: import and apply custom logging configuration in settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 
 import environ
 
+from .logging import LOGGING
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,6 +56,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
+LOGGING = LOGGING
 
 # Application definition
 
