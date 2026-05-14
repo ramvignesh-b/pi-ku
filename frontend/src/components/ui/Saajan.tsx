f0c6b608fd6426fd4b4397343c31a663151f171c
me <ramvignesh-b@github.com>
2026-05-14 06:00:01 +0530

    style: adjust Saajan image width

--- frontend/src/components/ui/Saajan.tsx
+++ frontend/src/components/ui/Saajan.tsx
@@ -2,52 +2,52 @@ import { useEffect, useState } from "react";
 import sf_mini from "../../assets/sf_mini.png";
 
 interface SaajanProps {
-  message: string;
-  position?: "top" | "left" | "right" | "bottom";
+    message: string;
+    position?: "top" | "left" | "right" | "bottom";
 }
 
 export default function Saajan({ message, position = "right" }: SaajanProps) {
-  const [animate, setAnimate] = useState<boolean>(false);
-  const [tooltipPosition, setTooltipPosition] =
-    useState<string>("tooltip-right");
-  const [alignment, setAlignment] = useState<string>("justify-start");
+    const [animate, setAnimate] = useState<boolean>(false);
+    const [tooltipPosition, setTooltipPosition] =
+        useState<string>("tooltip-right");
+    const [alignment, setAlignment] = useState<string>("justify-start");
 
-  useEffect(() => {
-    setAnimate(true);
-    const timeout = setTimeout(() => {
-      setAnimate(false);
-    }, 1000);
+    useEffect(() => {
+        setAnimate(true);
+        const timeout = setTimeout(() => {
+            setAnimate(false);
+        }, 1000);
 
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, []);
 
-  useEffect(() => {
-    setTooltipPosition(`tooltip-${position}`);
-    if (position === "top") {
-      setAlignment("justify-center");
-    }
-    if (position === "right") {
-      setAlignment("justify-start");
-    }
-    if (position === "left") {
-      setAlignment("justify-end");
-    }
-  }, [position]);
+    useEffect(() => {
+        setTooltipPosition(`tooltip-${position}`);
+        if (position === "top") {
+            setAlignment("justify-center");
+        }
+        if (position === "right") {
+            setAlignment("justify-start");
+        }
+        if (position === "left") {
+            setAlignment("justify-end");
+        }
+    }, [position]);
 
-  return (
-    <div className={`relative w-full flex ${alignment}`}>
-      <div
-        className={`tooltip tooltip-open ${tooltipPosition} before:border before:border-dashed before:border-primary/40 before:max-w-xs before:whitespace-pre-line italic before:text-left`}
-        data-tip={message}
-      >
-        <img
-          src={sf_mini}
-          alt="saajan"
-          className={`sepia-20 w-35 -mb-6 ${animate ? "animate-[pulse_.5s_ease_2]" : ""}`}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className={`relative w-full flex ${alignment}`}>
+            <div
+                className={`tooltip tooltip-open ${tooltipPosition} before:border before:border-dashed before:border-primary/40 before:max-w-xs before:whitespace-pre-line italic before:text-left`}
+                data-tip={message}
+            >
+                <img
+                    src={sf_mini}
+                    alt="saajan"
+                    className={`sepia-20 w-30 -mb-6 ${animate ? "animate-[pulse_.5s_ease_2]" : ""}`}
+                />
+            </div>
+        </div>
+    );
 }
